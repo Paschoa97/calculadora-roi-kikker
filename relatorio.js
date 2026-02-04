c401534ffce6ecb6ce99b199a8bbbb36d79978ab
Paschoa97 <100591738+Paschoa97@users.noreply.github.com>
2026-02-04 18:47:47 -0300

    Update relatorio.js

--- relatorio.js
+++ relatorio.js
@@ -1,17 +1,17 @@
 let dados = JSON.parse(localStorage.getItem("relatorioROI"));
 
-document.getElementById("infoLead").innerText =
-  `Cliente: ${dados.nome} | Email: ${dados.email}`;
-
 function moeda(valor){
   return valor.toLocaleString("pt-BR",{style:"currency",currency:"BRL"});
 }
 
+document.getElementById("clienteInfo").innerText =
+  `Cliente: ${dados.nome} | Rede: ${dados.rede} | Email: ${dados.email}`;
+
 document.getElementById("fatAnual").innerText = moeda(dados.faturamentoAnual);
-document.getElementById("margemOut").innerText = dados.margem + "%";
-document.getElementById("lojasOut").innerText = dados.lojas;
-document.getElementById("cdsOut").innerText = dados.cds;
-document.getElementById("itensOut").innerText = dados.itens;
+document.getElementById("margem").innerText = dados.margem + "%";
+document.getElementById("lojas").innerText = dados.lojas;
+document.getElementById("cds").innerText = dados.cds;
+document.getElementById("itens").innerText = dados.itens;
 
 document.getElementById("ganhoComercial").innerText = moeda(dados.ganhoComercial);
 document.getElementById("ganhoOperacional").innerText = moeda(dados.ganhoOperacional);
@@ -19,4 +19,5 @@ document.getElementById("ganhoQuebras").innerText = moeda(dados.ganhoQuebras);
 document.getElementById("ganhoEstoque").innerText = moeda(dados.ganhoEstoque);
 
 document.getElementById("roiFinal").innerText = dados.roi + "x";
-document.getElementById("alivioFinal").innerText = "Alívio de Caixa: " + moeda(dados.alivioCaixa);
+document.getElementById("alivioCaixa").innerText =
+  "Alívio de Caixa: " + moeda(dados.alivioCaixa);
