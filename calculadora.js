c053f8bc227bf9e34ece8cc5f764434e11a4f7ab
Paschoa97 <100591738+Paschoa97@users.noreply.github.com>
2026-03-20 15:47:52 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -19,3 +19,20 @@ document.getElementById("verRelatorio").addEventListener("click", () => {
 
   window.location.href = "relatorio.html";
 });
+
+<!-- Event snippet for Contato conversion page
+In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
+<script>
+function gtag_report_conversion(url) {
+  var callback = function () {
+    if (typeof(url) != 'undefined') {
+      window.location = url;
+    }
+  };
+  gtag('event', 'conversion', {
+      'send_to': 'AW-11176537357/ZSNzCJ2m_bIYEI3asdEp',
+      'event_callback': callback
+  });
+  return false;
+}
+</script>
